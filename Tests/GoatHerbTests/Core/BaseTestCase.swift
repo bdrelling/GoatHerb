9d96666b23365ab56b7bcf893ee5e417c36a2438
Brian Drelling <brian@drelling.com>
2022-09-02 15:36:47 -0500

    Added debug logging

--- Tests/GoatHerbTests/Core/BaseTestCase.swift
+++ Tests/GoatHerbTests/Core/BaseTestCase.swift
@@ -7,15 +7,11 @@ import Logging
 import XCTest
 
 class BaseTestCase: XCTestCase {
-    let dispatcher: NetworkRequestDispatching = .universal
+    private let dispatcher: NetworkRequestDispatching = .universal
 
-    let logger: Logger? = {
-        if ProcessInfo.processInfo.environment["CI"] == nil {
-            return .debug("GoatHerbTests")
-        } else {
-            return nil
-        }
-    }()
+    /// Our debug logger, which will print JSON responses to the terminal.
+    /// There is no private data accessible, as the only access tokens used for testing are restricted, read-only tokens.
+    private let logger: Logger = .debug("GoatHerbTests")
 
     /// A GitHub client that detects an access token from the environment.
     private(set) lazy var gitHub = GitHub(dispatcher: self.dispatcher, logger: self.logger)
