c0dcf7138720b7c5ad1c7eca04b94c85b21055e7
Brian Drelling <brian@drelling.com>
2023-01-11 19:49:11 -0600

    Removed KippleTools from Package.swift

--- Package.swift
+++ Package.swift
@@ -35,10 +35,3 @@ let package = Package(
         ),
     ]
 )
-
-#if swift(>=5.5)
-// Add Kipple Tools if possible.
-package.dependencies.append(
-    .package(url: "https://github.com/swift-kipple/Tools", from: "0.3.0")
-)
-#endif
